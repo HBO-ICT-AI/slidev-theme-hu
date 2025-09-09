b7fba13e5868795a694335292d8ee2225a82007e
Brian van der Bijl <peikos@peikos.net>
2025-09-09 11:18:35 +0200

    Deprecation warning

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -2,9 +2,5 @@ import { defineShikiSetup } from '@slidev/types';
 
 export default defineShikiSetup(async ({ loadTheme }) => {
   return {
-    theme: {
-      dark: await loadTheme(require.resolve('./monokai.json')),
-      light: await loadTheme(require.resolve('./monokai.json')),
-    },
   };
 });
